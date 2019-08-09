787db3fd85cc89580b8bce9ed0f285560deaea66
harujoh <harujoh.yuki@gmail.com>
2019-08-09 14:19:09 +0900

    ファーストコミット

---
+++ NChainer/Chainer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using NConstrictor;
+
+namespace NChainer
+{
+    public class Chainer
+    {
+        private static PyObject _chainer;
+        public static PyObject Variable;
+        public static PyObject Functions;
+        public static PyObject Links;
+        public static PyObject Optimizers;
+        public static PyObject Config;
+
+        public static void Initialize()
+        {
+            _chainer = PyImport.ImportModule("chainer");
+
+            if (_chainer == IntPtr.Zero)
+            {
+                throw new Exception("chainer failed to import");
+            }
+
+            Variable = _chainer["Variable"];
+            Links = _chainer["links"];
+            Functions = _chainer["functions"];
+            Optimizers = _chainer["optimizers"];
+            Config = _chainer["config"]; 
+        }
+    }
+}
